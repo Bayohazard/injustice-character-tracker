08b6acc07b170b5f706488842031ec966b0ebf2b
Bryan Paez <bryanpaez2014@yahoo.com>
2022-11-06 00:42:20 -0700

    Update opacity with CSS instead of JS

--- views/script.js
+++ views/script.js
@@ -1,11 +1,11 @@
 const checkboxes = document.querySelectorAll('.character-obtained-checkbox');
 
-checkboxes.forEach((checkbox) => {
-    checkbox.addEventListener('click', (e) => {
-        const rowName = e.target.parentElement.id;
-        const characterName = document.querySelector(`#${rowName} .character-name`);
-
-        characterName.classList.toggle('faded');
-        checkbox.classList.toggle('checked');
-    });
-});
+// checkboxes.forEach((checkbox) => {
+//     checkbox.addEventListener('click', (e) => {
+//         const rowName = e.target.parentElement.id;
+//         const characterName = document.querySelector(`#${rowName} .character-name`);
+//
+//         characterName.classList.toggle('faded');
+//         checkbox.classList.toggle('checked');
+//     });
+// });
