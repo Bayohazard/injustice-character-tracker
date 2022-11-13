654a4ee26932d70e2893c3129fe60103cc9423d5
Bryan Paez <bryanpaez2014@yahoo.com>
2022-11-13 14:49:51 -0800

    Make character stats section work when clicked

--- views/script.js
+++ views/script.js
@@ -1,17 +1,30 @@
 const characterList = document.getElementById('character-list-characters');
 
 characterList.addEventListener('click', (e) => {
+    if (e.target.type === 'checkbox' && !e.target.checked) {
+        const id = e.target.parentElement.parentElement.id;
+        document.querySelector(`#${id} .character-stats`).classList.add('hide');
+        document.querySelector(`#${id}`).classList.remove('character-row-expanded');
+        return;
+    }
+
+    if (e.target.type === 'checkbox') {
+        return;
+    }
+
     const classes = e.target.classList;
     let id = '';
 
     if (classes[0] === 'character-overview') {
         id = e.target.parentElement.id;
-        console.log('1: ', id);
     }
 
     if (classes[0] === 'character-name') {
         id = e.target.parentElement.parentElement.parentElement.id;
-        console.log('2: ', e.target.parentElement.parentElement.parentElement.id);
+    }
+
+    if (classes[0] === 'character-name-wrapper') {
+        id = e.target.parentElement.parentElement.id;
     }
 
     if (document.querySelector(`#${id} .character-obtained-checkbox`).checked) {
