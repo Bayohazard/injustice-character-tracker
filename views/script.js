28299e52ec256cee9e38acb1276b4ad5df0bfa20
Bryan Paez <bryanpaez2014@yahoo.com>
2022-11-13 13:23:34 -0800

    Add promotion/level section to each character

--- views/script.js
+++ views/script.js
@@ -1,11 +1,21 @@
-const checkboxes = document.querySelectorAll('.character-obtained-checkbox');
+const characterList = document.getElementById('character-list-characters');
 
-// checkboxes.forEach((checkbox) => {
-//     checkbox.addEventListener('click', (e) => {
-//         const rowName = e.target.parentElement.id;
-//         const characterName = document.querySelector(`#${rowName} .character-name`);
-//
-//         characterName.classList.toggle('faded');
-//         checkbox.classList.toggle('checked');
-//     });
-// });
+characterList.addEventListener('click', (e) => {
+    const classes = e.target.classList;
+    let id = '';
+
+    if (classes[0] === 'character-overview') {
+        id = e.target.parentElement.id;
+        console.log('1: ', id);
+    }
+
+    if (classes[0] === 'character-name') {
+        id = e.target.parentElement.parentElement.parentElement.id;
+        console.log('2: ', e.target.parentElement.parentElement.parentElement.id);
+    }
+
+    if (document.querySelector(`#${id} .character-obtained-checkbox`).checked) {
+        document.querySelector(`#${id} .character-stats`).classList.toggle('hide');
+        document.querySelector(`#${id}`).classList.toggle('character-row-expanded');
+    }
+});
